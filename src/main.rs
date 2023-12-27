181b832ad3dd83f6c307cc920385e9248f51006d
liuuu <57059329@qq.com>
2023-12-27 17:55:52 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod formatted_print;
+
+
+fn main() {
+    formatted_print::formatted_print_demo();
+    formatted_print::formatted_print_debug();
+    formatted_print::formatted_pretty_print();
+    formatted_print::formatted_display();
+    formatted_print::test_formatted_vec();
+
+}
